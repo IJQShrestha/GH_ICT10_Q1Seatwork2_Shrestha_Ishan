e4894b8ad102bd43c886f5c6c5322db17ae39aed
IJQShrestha <shrestha.iq@obmontessori.edu.ph>
2025-09-08 20:07:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Data types in Python
+from pyscript import display
+
+restaurant_name = 'Mandala' # string
+owner_name =  'Owner: Ishan Shrestha'  # string
+year_established = 'EST 2020' # integer
+popular_item_price = 'P180' # integer
+has_delivery = True # boolean
+product_names = ['Butter Chicken', 'Beef Kebab', 'Chicken Biryani', 'Chicken Tandoori', 'Beef Biryani'] # list
+business_hours = '11am-8pm' # integer
+menu_prices = ['P180', 'P130', 'P210', 'P150', 'P200',] # list
+common_allergens = 'Peanuts' # string
+tax_rate = 0.78 # float
+
+
+display(f'{restaurant_name}', target='restaurant')
+display(f'{owner_name}', target='owner')
+display(f'{year_established}', target='year')
